817cc205343e88d68ff18e8c4dc246fb97979791
Salavat Magazov <s.magazov@gmail.com>
2019-03-11 22:14:24 +0000

    ah... I found out what .settings/ folder in eclipse project does.

--- Inc/main.h
+++ Inc/main.h
@@ -90,6 +90,8 @@ void Error_Handler(void);
 /* Private defines -----------------------------------------------------------*/
 #define B1__Blue_PushButton_Pin GPIO_PIN_13
 #define B1__Blue_PushButton_GPIO_Port GPIOC
+#define LD2_Pin GPIO_PIN_5
+#define LD2_GPIO_Port GPIOA
 /* USER CODE BEGIN Private defines */
 
 /* USER CODE END Private defines */
